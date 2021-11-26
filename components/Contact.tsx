82e6e65440352204437d946507124ac5e7045632
Paulin Caratini <pcaratini@icloud.com>
2021-11-26 15:28:03 +0100

    contact form validation (wip)

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -11,6 +11,15 @@ import { Spinner } from "./Spinner";
 import styles from "../styles/Contact.module.scss";
 
 /* -------------------------------------------------------------------------- */
+/*                                 INTERFACES                                 */
+/* -------------------------------------------------------------------------- */
+
+interface IInput {
+  value: string;
+  error: string;
+}
+
+/* -------------------------------------------------------------------------- */
 /*                                  COMPONENT                                 */
 /* -------------------------------------------------------------------------- */
 export const Contact = ({
@@ -21,15 +30,14 @@ export const Contact = ({
   /* -------------------------------------------------------------------------- */
   /*                                 REACT STATE                                */
   /* -------------------------------------------------------------------------- */
+  const [fullname, setFullname] = useState<IInput>({ value: "", error: "" });
+  const [email, setEmail] = useState<IInput>({ value: "", error: "" });
+  const [message, setMessage] = useState<IInput>({ value: "", error: "" });
   const [isSending, setIsSending] = useState<boolean>(false);
-  const [fullname, setFullname] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [message, setMessage] = useState<string>("");
 
   /* -------------------------------------------------------------------------- */
   /*                                  FUNCTIONS                                 */
   /* -------------------------------------------------------------------------- */
-
   /**
    * Send email
    *
@@ -38,13 +46,26 @@ export const Contact = ({
    */
   async function sendMail(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    checkFullname();
+    checkEmail();
+    checkMessage();
+
+    if (fullname.error != "" || email.error != "" || message.error != "") {
+      return;
+    }
+
     setIsSending(true);
+
+    //
+    console.log("end");
+    return;
 
     const res = await fetch("/api/sendgrid", {
       body: JSON.stringify({
-        fullname,
-        email,
-        message,
+        fullname: fullname.value,
+        email: email.value,
+        message: message.value,
       }),
       headers: {
         "Content-Type": "application/json",
@@ -74,6 +95,71 @@ export const Contact = ({
     setShowModalContact(false);
   }
 
+  /**
+   * Check fullname input
+   */
+  function checkFullname() {
+    // Initialization
+    let error: string = "";
+    // Validation
+    if (fullname.value.length === 0) {
+      error = "Vous devez remplir ce champs";
+    } else if (fullname.value.length < 3) {
+      error = "Le champs doit faire au moins 3 caractères";
+    } else if (fullname.value.length > 29) {
+      error = "Le champs doit faire moins de 30 caractères";
+    }
+    // Assignation
+    setFullname({
+      ...fullname,
+      error,
+    });
+  }
+
+  /**
+   * Check email input
+   */
+  function checkEmail() {
+    // Initialization
+    let error: string = "";
+    // Validation
+    if (email.value.length === 0) {
+      error = "Vous devez remplir ce champs";
+    } else if (email.value.length > 29) {
+      error = "Le champs doit faire moins de 30 caractères";
+    } else if (
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)
+    ) {
+      error = "Veuillez renseigner une adresse email valide";
+    }
+    // Assignation
+    setEmail({
+      ...email,
+      error,
+    });
+  }
+
+  /**
+   * Check message textarea
+   */
+  function checkMessage() {
+    console.log("checked message");
+
+    // Initialization
+    let error: string = "";
+    // Validation
+    if (message.value.length < 15) {
+      error = "Expliquez brièvement l'objet de votre demande";
+    } else if (message.value.length > 9999) {
+      error = "Le champs doit faire moins de 1000 caractères";
+    }
+    // Assignation
+    setMessage({
+      ...message,
+      error,
+    });
+  }
+
   /* -------------------------------------------------------------------------- */
   /*                                  TEMPLATE                                  */
   /* -------------------------------------------------------------------------- */
@@ -92,34 +178,60 @@ export const Contact = ({
           </p>
         </div>
         <form onSubmit={(e) => sendMail(e)}>
-          <div>
+          {/* -------------------------------- FULLNAME -------------------------------- */}
+          <div className={styles.formGroup}>
             <label htmlFor="fullname">Prénom et nom</label>
             <input
               type="text"
               id="fullname"
-              value={fullname}
-              onChange={(e) => setFullname(e.target.value)}
+              value={fullname.value}
+              onChange={(e) =>
+                setFullname({
+                  ...fullname,
+                  value: e.target.value,
+                })
+              }
+              onBlur={checkFullname}
             />
+            <div className={styles.errors}>{fullname.error}</div>
           </div>
 
-          <div>
+          {/* ---------------------------------- EMAIL --------------------------------- */}
+          <div className={styles.formGroup}>
             <label htmlFor="email">Email professionnel</label>
             <input
               type="email"
               id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={email.value}
+              onChange={(e) =>
+                setEmail({
+                  ...email,
+                  value: e.target.value,
+                })
+              }
+              onBlur={checkEmail}
             />
+            <div className={styles.errors}>{email.error}</div>
           </div>
 
-          <div>
+          {/* --------------------------------- MESSAGE -------------------------------- */}
+          <div className={styles.formGroup}>
             <label htmlFor="message">Votre besoin en quelques mots</label>
             <textarea
               id="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
+              value={message.value}
+              onChange={(e) =>
+                setMessage({
+                  ...message,
+                  value: e.target.value,
+                })
+              }
+              onBlur={checkMessage}
             ></textarea>
+            <div className={styles.errors}>{message.error}</div>
           </div>
+
+          {/* ------------------------------ SUBMIT BUTTON ----------------------------- */}
           <button type="submit" disabled={isSending}>
             {isSending ? <Spinner /> : "Envoyer"}
           </button>
