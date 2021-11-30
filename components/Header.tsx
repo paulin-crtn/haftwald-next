db5146d6f1ea01e069a7e95559b4c598ffe898d6
Paulin Caratini <pcaratini@icloud.com>
2021-11-30 19:56:31 +0100

    menu burger (wip)

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,48 +17,103 @@ import styles from "../styles/Header.module.scss";
 /*                                  COMPONENT                                 */
 /* -------------------------------------------------------------------------- */
 export const Header = () => {
-  /* ------------------------------- REACT STATE ------------------------------ */
+  /* -------------------------------------------------------------------------- */
+  /*                                 REACT STATE                                */
+  /* -------------------------------------------------------------------------- */
   const [showModalContact, setShowModalContact] = useState<boolean>(false);
+
+  /* -------------------------------------------------------------------------- */
+  /*                                  FUNCTIONS                                 */
+  /* -------------------------------------------------------------------------- */
+  /**
+   *
+   */
+  function toggleNavigationMobile() {
+    const navigationMobile =
+      document.getElementsByClassName("navigationMobile")[0];
+    if (navigationMobile) {
+      if (navigationMobile.classList.contains("active")) {
+        navigationMobile.classList.remove("active");
+      } else {
+        navigationMobile.classList.add("active");
+      }
+    }
+  }
 
   /* -------------------------------------------------------------------------- */
   /*                                  TEMPLATE                                  */
   /* -------------------------------------------------------------------------- */
   return (
-    <header className={styles.container}>
-      <div className={styles.wrapper}>
-        <Link href="/" passHref>
-          <a className={styles.logo}>
-            Haftwald<span className={styles.dot}>.</span>
-          </a>
-        </Link>
-        <nav className={styles.navigation}>
-          <ul>
-            <li>
-              <a href="#services">Prestations</a>
-            </li>
-            <li>
-              <a href="#organisation">Organisation</a>
-            </li>
-            <li>
-              <a href="#questions">Questions</a>
-            </li>
-            <li>
-              <a href="#commitments">Engagements</a>
-            </li>
-            <li>
-              <a href="#about">Qui suis-je ?</a>
-            </li>
-            <li>
-              <button
-                className={styles.cta}
-                onClick={() => setShowModalContact(true)}
-              >
-                Contact
-              </button>
-            </li>
-          </ul>
-        </nav>
+    <header>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <Link href="/" passHref>
+            <a className={styles.logo}>
+              Haftwald<span className={styles.dot}>.</span>
+            </a>
+          </Link>
+          <nav className={styles.navigation}>
+            <ul>
+              <li>
+                <a href="#services">Prestations</a>
+              </li>
+              <li>
+                <a href="#organisation">Organisation</a>
+              </li>
+              <li>
+                <a href="#questions">Questions</a>
+              </li>
+              <li>
+                <a href="#commitments">Engagements</a>
+              </li>
+              <li>
+                <a href="#about">Qui suis-je ?</a>
+              </li>
+              <li>
+                <button
+                  className={styles.cta}
+                  onClick={() => setShowModalContact(true)}
+                >
+                  Contact
+                </button>
+              </li>
+            </ul>
+          </nav>
+          <div
+            className={styles.menuBurgerIcon}
+            onClick={toggleNavigationMobile}
+          >
+            menu
+          </div>
+        </div>
       </div>
+      <nav className="navigationMobile">
+        <ul onClick={toggleNavigationMobile}>
+          <li>
+            <a href="#services">Prestations</a>
+          </li>
+          <li>
+            <a href="#organisation">Organisation</a>
+          </li>
+          <li>
+            <a href="#questions">Questions</a>
+          </li>
+          <li>
+            <a href="#commitments">Engagements</a>
+          </li>
+          <li>
+            <a href="#about">Qui suis-je ?</a>
+          </li>
+          <li>
+            <button
+              className={styles.cta}
+              onClick={() => setShowModalContact(true)}
+            >
+              Contact
+            </button>
+          </li>
+        </ul>
+      </nav>
       {/* ------------------------------------------------------------------------- */
       /*                                MODAL CONTACT                               */
       /* -------------------------------------------------------------------------- */}
