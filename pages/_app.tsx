9cc910d8ac285c0d3fef85042ee53a3c9faa9aa9
Paulin Caratini <pcaratini@icloud.com>
2021-12-04 03:21:38 +0100

    dynamic header border color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@
 /* -------------------------------------------------------------------------- */
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import "../styles/globals.scss";
 
 /* -------------------------------------------------------------------------- */
@@ -15,6 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
+      <Script src="js/headerBorderColor.js"></Script>
     </>
   );
 }
---
+++ public/js/headerBorderColor.js
@@ -0,0 +1,21 @@
+/* -------------------------------------------------------------------------- */
+/*                             HEADER BORDER COLOR                            */
+/* -------------------------------------------------------------------------- */
+
+window.onscroll = function () {
+  // Get header container
+  const headerContainer = document.querySelector(
+    "header div:first-child" // NextJS uses dynamic class so we target the container with :first-child
+  );
+
+  // Check scroll position
+  if (headerContainer) {
+    headerContainer.style.borderBottomWidth = "1px";
+    headerContainer.style.borderBottomStyle = "solid";
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      headerContainer.style.borderBottomColor = "#cccccc";
+    } else {
+      headerContainer.style.borderBottomColor = "transparent";
+    }
+  }
+};
