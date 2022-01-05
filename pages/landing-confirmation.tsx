4147fb6cb8a05de2167afe76a0fc745d672de986
Paulin Caratini <pcaratini@icloud.com>
2022-01-05 10:25:39 +0100

    Add footer to landing page confirmation + optimisation

--- pages/landing-confirmation.tsx
+++ pages/landing-confirmation.tsx
@@ -22,61 +22,72 @@ const LandingConfirmation: NextPage = () => {
   /* -------------------------------------------------------------------------- */
 
   return (
-    <div className={styles.pageIllustration}>
-      <div className={styles.pageBackground}>
-        {/* ------------------------------------------------------------------------ */
-        /*                                  HTML HEAD                                 */
-        /* -------------------------------------------------------------------------- */}
-        <Head>
-          <title>Confirmation envoi demande de contact | Haftwald</title>
-          <meta
-            name="description"
-            content="Confirmation envoi demande de contact."
-          />
-        </Head>
+    <div className={styles.pageBackground}>
+      {/* ------------------------------------------------------------------------ */
+      /*                                  HTML HEAD                                 */
+      /* -------------------------------------------------------------------------- */}
+      <Head>
+        <title>Confirmation envoi demande de contact | Haftwald</title>
+        <meta
+          name="description"
+          content="Confirmation envoi demande de contact."
+        />
+      </Head>
 
-        {/* ------------------------------------------------------------------------- */
-        /*                                    MAIN                                    */
-        /* -------------------------------------------------------------------------- */}
-        <main className={styles.container}>
-          <section className={styles.wrapper}>
-            <div className={styles.logoContainer}>
+      {/* ------------------------------------------------------------------------- */
+      /*                                    MAIN                                    */
+      /* -------------------------------------------------------------------------- */}
+      <main className={styles.container}>
+        <section className={styles.wrapper}>
+          <div className={styles.logoContainer}>
+            <Link href="/" passHref>
+              <a>
+                <figure className={styles.logo}>
+                  <Image
+                    src={logo}
+                    alt="logo haftwald développeur web freelance"
+                    quality={100}
+                  />
+                </figure>
+              </a>
+            </Link>
+          </div>
+          <h1>Merci pour votre demande</h1>
+          <p>
+            Nous vous recontacterons sous 2 jours ouvrés afin d’échanger sur
+            votre projet.
+          </p>
+          <div className={styles.buttons}>
+            <button>
               <Link href="/" passHref>
-                <a>
-                  <figure className={styles.logo}>
-                    <Image
-                      src={logo}
-                      alt="logo haftwald développeur web freelance"
-                      quality={100}
-                    />
-                  </figure>
-                </a>
+                Visiter le site
               </Link>
-            </div>
-            <h1>Merci pour votre demande</h1>
-            <p>
-              Nous vous recontacterons sous 2 jours ouvrés afin d’échanger sur
-              votre projet.
-            </p>
-            <div className={styles.buttons}>
-              <button>
-                <Link href="/" passHref>
-                  Visiter le site
-                </Link>
-              </button>
-              <button>
-                <Link href="https://www.google.com" passHref>
-                  Google
-                </Link>
-              </button>
-            </div>
-          </section>
-        </main>
+            </button>
+            <button>
+              <Link href="https://www.google.com" passHref>
+                Google
+              </Link>
+            </button>
+          </div>
+        </section>
+      </main>
 
-        {/* ------------------------------------------------------------------------- */
-        /*                                   FOOTER                                   */
-        /* -------------------------------------------------------------------------- */}
-      </div>
+      {/* ------------------------------------------------------------------------- */
+      /*                                   FOOTER                                   */
+      /* -------------------------------------------------------------------------- */}
+      <footer>
+        <p className={styles.legal}>
+          <span>
+            <Link href="/mentions-legales">Mentions légales</Link>
+          </span>{" "}
+          |{" "}
+          <span>
+            <Link href="/politique-de-confidentialite">
+              Politique de confidentialité & Cookies
+            </Link>
+          </span>
+        </p>
+      </footer>
     </div>
   );
 };
