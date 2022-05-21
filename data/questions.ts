da29f7135728f1bd8ffc1891db159266fb325a4d
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-05-21 17:25:38 +0200

    feat(question): update text

--- data/questions.ts
+++ data/questions.ts
@@ -9,33 +9,29 @@ import { IQuestion } from "../interfaces/IQuestion";
 
 export const questions: IQuestion[] = [
   {
-    title: "Quelles sont vos disponibilités ?",
+    title: "Quels sont les frameworks que vous utilisez ?",
     answer:
-      "Tant qu'il est possible de me joindre via le formulaire de contact c'est que je suis disponible sous moins d'un mois.",
+      "J'utilise les frameworks React, NextJS et Angular pour le développement front-end ainsi que les frameworks Laravel et NestJS pour le développement back-end et la communication avec les bases de données (SQL ou NoSQL).",
   },
   {
-    title: "Combien de temps pouvez-vous nous rejoindre ?",
+    title: "Développez-vous des sites avec des CMS comme Wordpress ?",
     answer:
-      "J'accepte des missions dont la durée s'étend de quelques jours à plusieurs mois.",
-  },
-  {
-    title: "Quels sont les langages et frameworks que vous maîtrisez ?",
-    answer:
-      "Je suis spécialisé en développement front-end (Javascript, Typescript) et travaille principalement avec les frameworks React, NextJS et Angular. Je possède également des connaissances en développement back-end (Laravel) ainsi qu'en base de données (SQL et NoSQL).",
+      'Je peux développer des sites internet avec des CMS "headless" tel que Strapi ou Directus. Ces solutions techniques modernes et performantes, en plus de réduire le coût de développement, présentent un certain nombre d\'avantages en comparaison des CMS traditionnels comme Wordpress ou Drupal.',
   },
   {
     title: "De combien d'années d'expérience disposez-vous ?",
-    answer: "Je fais du développement web depuis plus de 2 ans.",
+    answer:
+      "Je développe des applications web depuis environ 3 ans, principalement sur les langages Javascript, Typescript et PHP.",
   },
   {
     title: "Quels sont vos tarifs ?",
     answer:
-      "En fonction de la durée et de la complexité de la mission, je facture entre 320€ et 360€ HT par jour de travail.",
+      "En fonction de la durée et de la complexité de la mission, je facture entre 300€ et 360€ HT par jour. Des forfaits sont possibles pour certains projets.",
   },
   {
     title:
       "Pourquoi recourir à un freelance plutôt qu'à une agence web ou une ESN ?",
     answer:
-      "Cela apporte un certain nombre d’avantages, dont notamment : des compétences plus adaptées à votre projet, des tarifs plus compétitifs ainsi qu’une disponibilité et une réactivité plus forte.",
+      "Un développeur freelance apporte un certain nombre d’avantages, dont notamment : des compétences plus adaptées au projet, des tarifs plus compétitifs ainsi qu’une disponibilité et une réactivité plus forte.",
   },
 ];
